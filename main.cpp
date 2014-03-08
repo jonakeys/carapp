fc74897583ce4a970095518467739135f396ee9a
Jonathan van der Steege <jonathan@debian>
2014-03-08 22:42:27 +0100

    Added comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/*
+  Carapp
+  by Jonathan van der Steege (08.03.2014)
+  
+  Application lets you input info of a car and print the output to the screen
+ */
+
 #include <fstream>
 #include <iostream>
 #include <limits>
@@ -10,6 +17,7 @@ using namespace std;
 
 int main()
 {
+    // Initializing variables
     bool GO = true;
     int choice = 0;
     vector<Car> Autos;
@@ -17,15 +25,16 @@ int main()
     int weight, length, maxspeed;
     string name;
     
+    // Main application loop
     while(GO) {
-	cout << "(1) Voeg auto toe\n"
+	cout << "(1) Voeg auto toe\n" // Menu
 	     << "(2) Print informatie\n"
 	     << "(3) Stoppen\n" 
 	     << "Maak je keuze: ";
 	cin >> choice;
 	switch (choice) {
 	case 1:
-	    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Enter the info of the car-item
 	    cout << "Naam: ";
 	    getline(cin, name);
 	    cout << "Gewicht(kg): ";
@@ -34,10 +43,10 @@ int main()
 	    cin >> length;
 	    cout << "Max snelheid(km/u): ";
 	    cin >> maxspeed;
-	    Autos.push_back(Car(weight, length, maxspeed, name));
+	    Autos.push_back(Car(weight, length, maxspeed, name)); // Add the info the vector Autos
 	    break;
 	case 2:
-	    if(Autos.size() != 0) {
+	    if(Autos.size() != 0) { // Print the data to the screen
 		for (it = Autos.begin(); it < Autos.end(); ++it) {
 		    cout << endl;
 		    it->printData();
@@ -47,7 +56,7 @@ int main()
 	    else
 		cout << "Er zijn nog geen auto's toegevoegd.\n";
 	    break;
-	case 3:
+	case 3: // Exit the application
 	    cout << "Tot ziens!\n";
 	    GO = false;
 	    break;
